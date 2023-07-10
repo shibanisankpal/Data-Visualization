f78556f6f728b9969834b69242ca9e9fc10e2b66
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-10 19:23:09 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -2,72 +2,67 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-def main():
-    st.title("Data Visualization with Streamlit")
-    
-    # Upload file
-    uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
-    
-    if uploaded_file is not None:
-        # Check file content length
-        content = uploaded_file.getvalue()
-        if len(content) < 100:
-            st.write("Invalid file. Please ensure the file is not empty or incomplete.")
-            return
-        
-        # Read file
-        data = read_data(uploaded_file)
-        
-        if data is not None:
-            # Display data
-            st.subheader("Data")
-            st.write(data)
-            
-            # Data visualization options
-            st.sidebar.subheader("Visualization Options")
-            plot_type = st.sidebar.selectbox("Select a plot type", ["Histogram", "Box Plot", "Scatter Plot"])
-            
-            # Perform visualization based on selected plot type
-            if plot_type == "Histogram":
-                histogram(data)
-            elif plot_type == "Box Plot":
-                box_plot(data)
-            elif plot_type == "Scatter Plot":
-                scatter_plot(data)
-        else:
-            st.write("Invalid file format. Please upload a CSV or Excel file.")
-            st.write("Uploaded file type:", uploaded_file.type)
-            st.write("File contents:")
-            st.write(uploaded_file.getvalue())
-            st.write("File length:", len(uploaded_file.getvalue()))
 
-def read_data(file):
-    if file.type == "csv":
-        try:
-            data = pd.read_csv(file, delimiter=",")
-        except pd.errors.ParserError:
-            data = pd.read_csv(file, delimiter=";")    
-    elif file.type == "xlsx":
-        data = pd.read_excel(file)
-    else:
-        data = None
-    return data
+def main():
+    st.title("Custom Dataset Dashboard")
+
+    # Load and explore the dataset
+    uploaded_file = st.file_uploader("Upload a CSV or Excel file")
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.subheader("Data")
+        st.write(data)
+
+        # Create visualizations
+        st.subheader("Data Visualizations")
+
+        visualization_options = {
+            "Histogram": histogram,
+            "Pie Chart": pie_chart,
+            "Scatter Plot": scatter_plot,
+            "Count Plot": count_plot
+        }
+
+        selected_options = []
+        for option in visualization_options.keys():
+            if st.checkbox(option):
+                selected_options.append(option)
+
+        if selected_options:
+            for option in selected_options:
+                st.subheader(option)
+                visualization_options[option](data)
 
 def histogram(data):
-    column = st.sidebar.selectbox("Select a column", data.columns)
-    plt.hist(data[column].dropna())
-    st.pyplot()
+    column_hist = st.selectbox("Select a column for histogram", data.columns)
+    fig_hist, ax_hist = plt.subplots()
+    ax_hist.hist(data[column_hist].dropna())
+    st.pyplot(fig_hist)
 
-def box_plot(data):
-    column = st.sidebar.selectbox("Select a column", data.columns)
-    sns.boxplot(data[column].dropna())
-    st.pyplot()
+def pie_chart(data):
+    column_pie = st.selectbox("Select a column for pie chart", data.columns)
+    counts = data[column_pie].value_counts()
+    fig_pie, ax_pie = plt.subplots()
+    ax_pie.pie(counts, labels=counts.index, autopct='%1.1f%%')
+    ax_pie.set_aspect('equal')
+    st.pyplot(fig_pie)
 
 def scatter_plot(data):
-    x_column = st.sidebar.selectbox("Select X-axis column", data.columns)
-    y_column = st.sidebar.selectbox("Select Y-axis column", data.columns)
-    plt.scatter(data[x_column], data[y_column])
-    st.pyplot()
+    column_x = st.selectbox("Select X-axis column", data.columns)
+    column_y = st.selectbox("Select Y-axis column", data.columns)
+    fig_scatter, ax_scatter = plt.subplots(figsize=(8, 6))
+    ax_scatter.scatter(data[column_x], data[column_y])
+    ax_scatter.set_xlabel(column_x)
+    ax_scatter.set_ylabel(column_y)
+    st.pyplot(fig_scatter)
+
+
+def count_plot(data):
+    column_count = st.selectbox("Select a column for count plot", data.columns)
+    fig_count, ax_count = plt.subplots(figsize=(10, 6))
+    sns.countplot(data[column_count])
+    st.pyplot(fig_count)
+
 
 if __name__ == "__main__":
     main()
