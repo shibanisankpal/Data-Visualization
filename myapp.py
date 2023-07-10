8bba1a90c9b610f012d031244b8ca4f1981b62c6
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-10 19:27:04 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -9,7 +9,14 @@ def main():
     # Load and explore the dataset
     uploaded_file = st.file_uploader("Upload a CSV or Excel file")
     if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
+        if uploaded_file.type == "text/csv":
+            data = pd.read_csv(uploaded_file, encoding='utf-8')
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            data = pd.read_excel(uploaded_file, engine='openpyxl')
+        else:
+            st.write("Invalid file format. Please upload a CSV or Excel file.")
+            return
+
         st.subheader("Data")
         st.write(data)
 
@@ -18,51 +25,44 @@ def main():
 
         visualization_options = {
             "Histogram": histogram,
-            "Pie Chart": pie_chart,
+            "Bar Plot": bar_plot,
             "Scatter Plot": scatter_plot,
             "Count Plot": count_plot
         }
 
         selected_options = []
+        selected_columns = []
         for option in visualization_options.keys():
             if st.checkbox(option):
                 selected_options.append(option)
+                column = st.selectbox(f"Select a column for {option}", data.columns)
+                selected_columns.append(column)
 
         if selected_options:
-            for option in selected_options:
+            for option, column in zip(selected_options, selected_columns):
                 st.subheader(option)
-                visualization_options[option](data)
+                visualization_options[option](data, column)
 
-def histogram(data):
-    column_hist = st.selectbox("Select a column for histogram", data.columns)
+def histogram(data, column):
     fig_hist, ax_hist = plt.subplots()
-    ax_hist.hist(data[column_hist].dropna())
+    ax_hist.hist(data[column].dropna())
     st.pyplot(fig_hist)
 
-def pie_chart(data):
-    column_pie = st.selectbox("Select a column for pie chart", data.columns)
-    counts = data[column_pie].value_counts()
-    fig_pie, ax_pie = plt.subplots()
-    ax_pie.pie(counts, labels=counts.index, autopct='%1.1f%%')
-    ax_pie.set_aspect('equal')
-    st.pyplot(fig_pie)
+def bar_plot(data, column):
+    fig_bar, ax_bar = plt.subplots(figsize=(10, 6))
+    sns.countplot(data[column])
+    st.pyplot(fig_bar)
 
-def scatter_plot(data):
-    column_x = st.selectbox("Select X-axis column", data.columns)
-    column_y = st.selectbox("Select Y-axis column", data.columns)
+def scatter_plot(data, column):
+    column_x = st.selectbox("Select X-axis column", data.select_dtypes(include="number").columns)
     fig_scatter, ax_scatter = plt.subplots(figsize=(8, 6))
-    ax_scatter.scatter(data[column_x], data[column_y])
-    ax_scatter.set_xlabel(column_x)
-    ax_scatter.set_ylabel(column_y)
+    sns.scatterplot(data=data, x=column_x, y=column)
     st.pyplot(fig_scatter)
 
-
-def count_plot(data):
-    column_count = st.selectbox("Select a column for count plot", data.columns)
+def count_plot(data, column):
     fig_count, ax_count = plt.subplots(figsize=(10, 6))
-    sns.countplot(data[column_count])
+    sns.countplot(data[column])
     st.pyplot(fig_count)
-
 
 if __name__ == "__main__":
     main()
