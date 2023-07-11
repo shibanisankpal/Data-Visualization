a15822bc9ba346b20a80d521a5143d62130c24a9
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-11 11:53:36 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -3,8 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Define color palettes for styling
+primary_color = "#4287f5"  # Blue
+secondary_color = "#f5a042"  # Orange
+background_color = "#f5f5f5"  # Light Gray
+
 def main():
+    st.set_page_config(
+        page_title="Custom Dataset Dashboard",
+        page_icon="📊",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+
     st.title("Custom Dataset Dashboard")
+    st.markdown(
+        """
+        <style>
+        .title {
+            color: %s;
+            text-align: center;
+        }
+        </style>
+        """
+        % primary_color,
+        unsafe_allow_html=True
+    )
 
     # Load and explore the dataset
     uploaded_file = st.file_uploader("Upload a CSV or Excel file")
@@ -18,7 +42,7 @@ def main():
             return
 
         st.subheader("Data")
-        st.write(data)
+        st.dataframe(data.style.set_properties(**{'background-color': background_color}))
 
         # Create visualizations
         st.subheader("Data Visualizations")
@@ -39,20 +63,21 @@ def main():
 
 def histogram(data, column):
     fig_hist, ax_hist = plt.subplots()
-    ax_hist.hist(data[column].dropna())
+    ax_hist.hist(data[column].dropna(), color=secondary_color)
     st.pyplot(fig_hist)
 
 def pie_chart(data, column):
     fig_pie, ax_pie = plt.subplots()
-    data[column].value_counts().plot.pie(autopct='%1.1f%%')
+    data[column].value_counts().plot.pie(autopct='%1.1f%%', colors=[secondary_color, "#7fcdbb", "#edf8b1", "#fe9929"])
     ax_pie.set_ylabel("")
     st.pyplot(fig_pie)
 
 def count_plot(data, column):
     fig_count, ax_count = plt.subplots()
-    sns.countplot(data[column])
+    sns.countplot(data[column], color=secondary_color)
     st.pyplot(fig_count)
 
 if __name__ == "__main__":
     main()
+
 
